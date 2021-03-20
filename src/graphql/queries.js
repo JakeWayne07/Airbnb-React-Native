908339d1644ed0de39acf9ea4e39624102a9f748
JakeWayne07 <waynemutai@gmail.com>
2021-03-20 12:17:36 +0300

    Amplify push Schema

---
+++ src/graphql/queries.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPost = /* GraphQL */ `
+  query GetPost($id: ID!) {
+    getPost(id: $id) {
+      id
+      title
+      description
+      image
+      type
+      bed
+      bedroom
+      maxGuests
+      oldPrice
+      newPrice
+      latitude
+      longitude
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listPosts = /* GraphQL */ `
+  query ListPosts(
+    $filter: ModelPostFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        description
+        image
+        type
+        bed
+        bedroom
+        maxGuests
+        oldPrice
+        newPrice
+        latitude
+        longitude
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
